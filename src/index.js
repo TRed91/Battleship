60b62e024007ffb2d69c32574ca5d9d71f783da3
Thomas Roth <thomas.roth@aon.at>
2024-07-04 17:20:24 +0200

    start creating interface

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './style.css';
 const Player = require('./playerGenerator');
 
+
+const DOMbody = document.querySelector('body');
+
+const gameInterface = document.createElement('div');
+
+const newBoardBtn = document.createElement('button');
+const startGameBtn = document.createElement('button');
+newBoardBtn.innerHTML = 'New Random Board';
+startGameBtn.innerHTML = 'Start Game';
+
+DOMbody.appendChild(gameInterface);
+gameInterface.appendChild(newBoardBtn);
+gameInterface.appendChild(startGameBtn);
+
+const player1board = document.createElement('div');
+
